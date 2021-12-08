2d0b5d42e4aa9df59120a82ae862e3eae48907ae
soonmac <55690712+soonmac@users.noreply.github.com>
2021-12-08 23:57:34 +0900

    subPage dressup 기능 추가

--- js/sub.js
+++ js/sub.js
@@ -38,12 +38,131 @@ checkBoxes.forEach((checkbox,i)=>{
     })
 })
 
+const dressupBtns = document.querySelectorAll(".dressup-btns > li")
+const charaCustom = document.querySelector(".chara__custom__wrap");
+const customTitle = document.querySelector(".chara__custom__title")
+const hairBtn = document.querySelector(".hair_btn")
+const charaParts = [...document.querySelectorAll(".chara-parts")];
+console.log(charaParts);
+let hairIndex = 1,
+    eyesIndex = 1,
+    mouthIndex = 1,
+    clothIndex = 1;
+let thumb = document.querySelector(".chara__custom__img > img")
 
-let photoIndex = 1;
-document.querySelector(".hair").addEventListener("click",()=>{
-    photoIndex++;
-    photoIndex %= 4;
-    if(photoIndex ==0) photoIndex = 1;
-    const hairImg = document.querySelector(".chara_hair");
-    hairImg.src= `img/character/hair_0${photoIndex}.png`
+//이미지의 링크를 바꾸는 함수
+function partsImgChange(id, index) {
+    console.log(id);
+    const partsImg = charaParts.find(element => element.dataset.value == id)
+    console.log(partsImg);
+    partsImg.src= `img/character/${id}_0${index}.png`
+    thumb.src = `img/character/${id}_0${index}.png`
+}
+//커스텀 창의 다음 버튼을 눌렀을 때 다음 사진으로 바뀌는 함수
+function nextPhoto(id) {
+    if (id== "hair") {
+        hairIndex++;
+        hairIndex %= 4;
+        if(hairIndex ==0) hairIndex = 1;
+        partsImgChange(id,hairIndex);
+    }
+    if (id == "eyes") {
+        eyesIndex++;
+        eyesIndex %= 11;
+        if(eyesIndex ==0) eyesIndex = 1;
+        partsImgChange(id,eyesIndex);
+    }
+    if (id == "mouth") {
+        mouthIndex++;
+        mouthIndex %= 11;
+        if(mouthIndex ==0) mouthIndex = 1;
+        partsImgChange(id,mouthIndex);
+    }
+    if (id == "cloth") {
+        clothIndex++;
+        clothIndex %= 4;
+        if(clothIndex ==0) clothIndex = 1;
+        partsImgChange(id,clothIndex);
+    }
+
+}
+//커스텀 창의 이전 버튼을 눌렀을 때 이전 사진으로 바뀌는 함수
+function prevPhoto(id) {
+    if (id== "hair") {
+        hairIndex--;
+        hairIndex %= 4;
+        if(hairIndex ==0) hairIndex = 1;
+        partsImgChange(id,hairIndex);
+    }
+    if (id == "eyes") {
+        eyesIndex--;
+        eyesIndex %= 11;
+        if(eyesIndex ==0) eyesIndex = 1;
+        partsImgChange(id,eyesIndex);
+    }
+    if (id == "mouth") {
+        mouthIndex--;
+        mouthIndex %= 11;
+        if(mouthIndex ==0) mouthIndex = 1;
+        partsImgChange(id,mouthIndex);
+    }
+    if (id == "cloth") {
+        clothIndex--;
+        clothIndex %= 4;
+        if(clothIndex ==0) clothIndex = 1;
+        partsImgChange(id,clothIndex);
+    }
+
+}
+//커스텀 창의 다음 버튼 클릭 이벤트
+document.querySelector(".custom--next").addEventListener("click",()=>{
+    const id = charaCustom.id;
+    nextPhoto(id);
 })
+//커스텀 창의 이전버튼 클릭 이벤트
+document.querySelector(".custom--prev").addEventListener("click",()=>{
+    const id = charaCustom.id;
+    prevPhoto(id)
+})
+
+//버튼 클릭 이벤트 : 커스텀 창이 뜨는 함수
+dressupBtns.forEach(btn => {
+    btn.addEventListener("click",(e)=>{
+        const partsName = e.target.dataset.parts;
+        customVisible(partsName)
+        btn.classList.toggle("on");
+    })
+    // 커스텀 창의 close 버튼을 눌렀을 때 창이 닫히고 li의 on 클래스를 빼는 함수 
+    document.querySelector(".close-btn").addEventListener("click",()=>{
+        btn.classList.remove("on");
+        charaCustom.classList.remove("visible")
+    })
+})
+
+
+
+// 커스텀 창에 해당 파츠 데이터를 전달해서 보여주는 함수
+function customVisible(partsName) {
+    charaCustom.id= partsName
+    const id = charaCustom.id;
+    customTitle.innerHTML = id;
+    console.log(id);
+    thumbUrl(id);
+    charaCustom.classList.toggle("visible")
+    
+}
+
+function thumbUrl(id) {
+    if (id== "hair") {
+        thumb.src = `img/character/${id}_0${hairIndex}.png`
+    }
+    if (id == "eyes") {
+        thumb.src = `img/character/${id}_0${eyesIndex}.png`
+    }
+    if (id == "mouth") {
+        thumb.src = `img/character/${id}_0${mouthIndex}.png`
+    }
+    if (id == "cloth") {
+        thumb.src = `img/character/${id}_0${clothIndex}.png`
+    }
+}
